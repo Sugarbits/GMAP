5382501ed7dbaa5a6b8d4c9fe0f3d24e8a050996
Sugarbits <b1030536039@gmail.com>
2017-07-23 20:25:29 +0800

    buck
    上傳到第三版，待測試

--- EX4.php
+++ EX4.php
@@ -9,11 +9,11 @@
     <meta charset="utf-8">
     <title>Simple markers</title>
 	<script>
-	var btn_value = null ;//for saving value
-	var markers = [];//save all markers
-	var info_old = null;
-	var marker_old = null;//to make history trace effect
+	var firsttime = true;//to make history trace effect
+	var btn_value;
 	var map;//
+	var markers = [];
+	var infos = [];
 	//現在的位置用圖
 	//var now_icon_url = 'http://maps.google.com/mapfiles/ms/icons/bus.png';	
 	var now_icon_url = 'pic/busicon.png';
@@ -24,7 +24,15 @@
   </head>
   <body>
    <!--標頭-->
-   <div id="foobar"  class='toggle' style='display:none;'></div>
+   <div id="foobar"  class='toggle' style='display:none;'>
+   
+		<div id="foobar_left">123</div>
+		<div id="foobar_right">
+		<div id='home' class='side'>&nbsp首&nbsp頁&nbsp</div>
+		<div id='close' class='side'>&nbsp關&nbsp閉&nbsp</div>
+		</div>
+   </div>
+
    <!--地圖主體-->
    <div id="map"></div>
    <!--附屬資訊_介紹欄位-->
@@ -35,6 +43,12 @@
       $( ".toggle" ).toggle(100);
     };
 
+	$('#home').on('click', function(){
+		panto_muti_marker(markers);
+	});
+	$('#close').on('click', function(){
+		$( ".toggle" ).hide(100,false); 
+	});
 	$('#map').on('click', function(){
 		console.log($('#foobar').is(":hidden"));
 	  if(($('#foobar').is(":hidden")) == true){
@@ -68,75 +82,73 @@
 	}
 	function renew(){//ajax抓值
 		$.getJSON( "crawler/cr_motc_bus.php", function( data ) {
-		$( "#foobar" ).html('');
-		var items = [];
-		$.each( data, function( key, val ) {
-			//items.push( "<li id='" + key + "'>" + val + "</li>" );
-			//console.log(data);
-			var car_no = data[key]['PlateNumb'];//頻繁使用車號
-			//data[key]['PlateNumb']
-			//$( "#foobar" ).append( "<div class='btn'>".123."</div>" );
-				$( "#foobar" ).append( "<div class='"+btn_css_render(car_no)+"' data-val='"+car_no+"'>&nbsp;&nbsp;"+car_no+"</div>" );//按鈕生成,觸發自訂義
-			//add_button(data[key]['PlateNumb']);
-			//if(markers_pro[data[key]['PlateNumb']] === undefined
-			if(car_no==btn_value){//弱水三千，只取一瓢飲
-			/*
-			console.log(data[key]);
-			console.log(data[key]['BusPosition']['PositionLat']);
-			console.log(data[key]['BusPosition']['PositionLon']);
-			console.log(data[key]['GPSTime']);
-			console.log(data[key]['PlateNumb']);
-			*/
-			//create gmap latlng obj
-			 tmpLatLng = {lat : data[key]['BusPosition']['PositionLat'],lng :　data[key]['BusPosition']['PositionLon']};//google map latlng obj 
-			var tmptitle = {name:data[key]['PlateNumb'],time:data[key]['GPSTime']};//google map marker.title 
-			var tmpcontent = "時速: " + data[key]['Speed'] +"km"+  '<br></h3>' + "車號" + car_no; 
-			
-			//給附屬資訊_內容
-			//$('#speed').html(data[key]['UpdateTime']);
-			$('#speed').html(data[key]['Speed']+"KM/S");
-			$('#car_name').html(car_no);
-			$('#latlng').html( data[key]['BusPosition']['PositionLat']+'<br>'+data[key]['BusPosition']['PositionLon']);
-			
-			if(marker_old == null){//first time add marker
-				console.log('第一筆，新增資料!');
+		$( "#foobar_left" ).html('');
+				if(firsttime == true){//第一次撈
+					firsttime = false;
+					}else{	
+					initail();//清除上一次資料
+				}
+				$.each( data, function( key, val ) {
+				//console.log(data);
+				var car_no = data[key]['PlateNumb'];//頻繁使用車號
+				//data[key]['PlateNumb']
+				$( "#foobar_left" ).append( "<div class='"+btn_css_render(car_no)+"' data-val='"+car_no+"'>&nbsp;&nbsp;"+car_no+"</div>" );//按鈕生成,觸發自訂義
+				//add_button(data[key]['PlateNumb']);
+				/*
+				console.log(data[key]);
+				console.log(data[key]['BusPosition']['PositionLat']);
+				console.log(data[key]['BusPosition']['PositionLon']);
+				console.log(data[key]['GPSTime']);
+				console.log(data[key]['PlateNumb']);
+				*/
+				//create gmap latlng obj
+				tmpLatLng = {lat : data[key]['BusPosition']['PositionLat'],lng :　data[key]['BusPosition']['PositionLon']};//google map latlng obj 
+				var tmptitle = {name:data[key]['PlateNumb'],time:data[key]['GPSTime']};//google map marker.title 
+				var tmpcontent = "時速: " + data[key]['Speed'] +"km"+  '<br></h3>' + "車號" + car_no; 
+				
+				//給附屬資訊_內容
+				//$('#speed').html(data[key]['UpdateTime']);
+				$('#speed').html(data[key]['Speed']+"KM");
+				$('#car_name').html(car_no);
+				$('#latlng').html( data[key]['BusPosition']['PositionLat']+'<br>'+data[key]['BusPosition']['PositionLon']);
 				var marker = add_marker(map,tmpLatLng,tmptitle,tmpcontent);
 				var info = add_info(map,tmpLatLng,tmpcontent);
-				/*marker.infowindow = new google.maps.InfoWindow(
+				marker.infowindow = new google.maps.InfoWindow(
 				{
 					content: tmpcontent
-				});*/
-				markers.push(marker);//write into history
-				marker_old = marker;//temp save for last one
-				info_old = info;
-				}else{
-					if(compare_latlng(marker_old,data[key]['BusPosition']['PositionLat'],data[key]['BusPosition']['PositionLon'])){//last one position the same,we not need renew
-						var info = add_info(map,tmpLatLng,tmpcontent);
-						var marker = add_marker(map,tmpLatLng,tmptitle,tmpcontent);
-						console.log('定位不相同，新增資料!');
-						marker_old.setIcon(past_icon_url);//turn to past sign
-						marker_old.setOptions({'opacity': 1});//opacity
-						marker_old = marker;
-						info_old.close();
-						info_old = info;						
-						//marker_old.setMap(null);
-					}else{
-						console.log('定位相同，無新資料');
-					}
-				}
-				
-			}
-				
-			});
+				});
+				markers.push(marker);
+				infos.push(info);
+				//
+			});//$.each END
 		});
 	}
 		function initail(){
 			for(index in markers){
 				markers[index].setMap(null);
 			}
+			for(index in markers){
+				infos[index].setMap(null);
+			}
+			infos = [];
 			markers = [];
-			marker_old = null;
-			info_old.close();
+			console.log('消除');
+			console.log(markers);
+		}
+		function panto_muti_marker(pmarkers){
+			if(pmarkers.length != 0){
+			var bounds = new google.maps.LatLngBounds();
+			for (var i = 0; i < pmarkers.length; i++) {
+				var point = new google.maps.LatLng(
+					parseFloat(pmarkers[i].getPosition().lat()),
+					parseFloat(pmarkers[i].getPosition().lng()));
+					// add each marker's location to the bounds
+					bounds.extend(point);
+			}
+			map.fitBounds(bounds);
+			}else{
+				alert('沒有公車！');
+			}
 		}
 		function add_info(a_map,a_latlng,a_content){
 			var infowindow = new google.maps.InfoWindow({                
@@ -155,12 +167,11 @@
 				icon : (now_icon_url)
 			});
 			 
-			map.panTo(tmpLatLng);
+			//map.panTo(tmpLatLng);
 			//bindInfoWindow(marker, map, infowindow, '<b>'+places[p].name + "</b><br>" + places[p].geo_name);
 			// not currently used but good to keep track of markers
 			//markers[data[key]['PlateNumb']].push(marker);
 			//http://maps.google.com/mapfiles/ms/icons/blue-dot.png
-			markers.push(marker);
 			console.log(markers);
 			return marker;
 		}
@@ -203,6 +214,39 @@
         //map.setCenter(chicago);
         });
 
+      }		
+		function CenterControl2(controlDiv, map) {
+
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'left';
+        controlUI.title = 'Click to recenter the map';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = 'home';
+        controlUI.appendChild(controlText);
+
+        // Setup the click event listeners: simply set the map to Chicago.
+		 map.controls[google.maps.ControlPosition.TOP_RIGHT ].push(centerControlDiv);
+        controlUI.addEventListener('click', function() {
+		panto_muti_marker(markers);
+        //map.setCenter(chicago);
+        });
+
       }
 	    var myLatLng = {lat: 23.7, lng: 121.4};
 
@@ -212,9 +256,11 @@
         });
 		// Create the DIV to hold the control and call the CenterControl()
         // constructor passing in this DIV.
+		
+
         var centerControlDiv = document.createElement('div');
-		centerControlDiv.index = 1;
-        var centerControl = new CenterControl(centerControlDiv, map);
+		centerControlDiv.index = 1; 
+		var centerControl = new CenterControl(centerControlDiv, map);
 
 	  renew();
       }
--- crawler/cr_motc_bus.php
+++ crawler/cr_motc_bus.php
@@ -3,10 +3,12 @@
     //$url = "https://www.thsrc.com.tw/tw/TimeTable/SearchResult";
     //$uri = "http://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeByFrequency/City/HualienCounty?$top=30&$format=JSON";
     //$uri = "https://www.zhihu.com/question/21471960";
+	//$url = "http://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeByFrequency/City/Taichung?$top=30&$format=JSON";//台中
+	$url = "http://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeByFrequency/City/HualienCounty?$top=30&$format=JSON";//花蓮
 	//init curl
 	$ch = curl_init();
 	//set curl options 設定你要傳送參數的目的地檔案
-	curl_setopt($ch, CURLOPT_URL, "http://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeByFrequency/City/HualienCounty?$top=30&$format=JSON");
+	curl_setopt($ch, CURLOPT_URL, $url);
 	//curl_setopt($ch, CURLOPT_URL, "http://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeByFrequency/City/Taipei?$top=30&$format=json");
 	curl_setopt($ch, CURLOPT_HEADER, false);   
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
